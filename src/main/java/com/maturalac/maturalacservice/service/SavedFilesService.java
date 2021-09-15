4e77b26e39a20218f84ab07d36b2ae7cb71d9432
Marko Jerkić <markojerkic266@gmail.com>
2021-09-15 22:29:56 +0200

    read path from properties file

--- src/main/java/com/maturalac/maturalacservice/service/SavedFilesService.java
+++ src/main/java/com/maturalac/maturalacservice/service/SavedFilesService.java
@@ -3,7 +3,8 @@ package com.maturalac.maturalacservice.service;
 import com.maturalac.maturalacservice.data.entity.SavedFile;
 import com.maturalac.maturalacservice.data.entity.util.FileType;
 import com.maturalac.maturalacservice.data.repository.SavedFileRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -12,15 +13,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SavedFilesService {
     private final SavedFileRepository savedFileRepository;
 
-    private final String BASEPATH = "C:\\Dev\\maturalac-service\\files\\";
+    @Value("${files.location}")
+    private String filesRootPath;
 
     public byte[] getFileByFileName(String fileName) throws IOException {
         SavedFile savedFile = this.savedFileRepository.findByFileName(fileName);
-        Path filePath = Paths.get(this.BASEPATH + fileName + (savedFile.getFileType() == FileType.IMAGE? ".png": ".mp3"));
+        Path filePath = Paths.get(this.filesRootPath + fileName + (savedFile.getFileType() == FileType.IMAGE? ".png": ".mp3"));
         return Files.readAllBytes(filePath);
     }
 }
